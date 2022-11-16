dd7468038908e49dec8294dd0c9cc834a774939c
awwa121 <65862943+awwa121@users.noreply.github.com>
2022-11-15 22:20:12 -0300

    Add files via upload

---
+++ Simulacion/multi.py
@@ -0,0 +1,14 @@
+import subprocess
+
+
+
+cantidad = int(input("Cantidad de simulaciones: "))
+lista = []
+for i in range(cantidad):
+    lista.append("Generador.py")
+procesos = [subprocess.Popen(["python3", simulacion]) for simulacion in tuple(lista)]
+
+
+for proceso in procesos:
+    proceso.wait()
+
