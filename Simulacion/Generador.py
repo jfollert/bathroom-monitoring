9ddba0929d47a50db701d3c24e4ab934f59c0d22
awwa121 <65862943+awwa121@users.noreply.github.com>
2022-11-15 22:22:07 -0300

    Update Generador.py

--- Simulacion/Generador.py
+++ Simulacion/Generador.py
@@ -13,18 +13,18 @@ now = datetime.now()
 inicio = time.time()
 fin = time.time()
 
-url = "https://wwocq05mxf.execute-api.sa-east-1.amazonaws.com/dev/sensors/36593e9d-f450-4958-bd5f-69b0e15e0765/records/"
+# Pueba con un uuid de sensor creado.
+#url = "https://wwocq05mxf.execute-api.sa-east-1.amazonaws.com/dev/sensors/36593e9d-f450-4958-bd5f-69b0e15e0765/records/"
+url = "https://wwocq05mxf.execute-api.sa-east-1.amazonaws.com/dev/sensors/"+str(sensor_uuid)+"/records/"
 
 while True:
     while(fin - inicio < 60):
         medicion = {
         "value": str(random.randint(0,10)),
         }
-
         medicion_uuid = uuid.uuid4()
         respuesta = requests.put(url+str(medicion_uuid), json=medicion)
         print(respuesta)
-
         fin = time.time()
         time.sleep(10)
     time.sleep(3540)
