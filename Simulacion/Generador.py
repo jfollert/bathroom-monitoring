558cda2ac1c58e1c9d2f3c7055b0edbd7ad33afb
awwa121 <65862943+awwa121@users.noreply.github.com>
2022-11-15 22:23:27 -0300

    Update Generador.py

--- Simulacion/Generador.py
+++ Simulacion/Generador.py
@@ -5,16 +5,12 @@ import time
 import uuid
 import requests
 
-# make a random UUID
-#UUID('bd65600d-8669-4903-8a14-af88203add38')
 sensor_uuid = uuid.uuid4()
 
 now = datetime.now()
 inicio = time.time()
 fin = time.time()
 
-# Pueba con un uuid de sensor creado.
-#url = "https://wwocq05mxf.execute-api.sa-east-1.amazonaws.com/dev/sensors/36593e9d-f450-4958-bd5f-69b0e15e0765/records/"
 url = "https://wwocq05mxf.execute-api.sa-east-1.amazonaws.com/dev/sensors/"+str(sensor_uuid)+"/records/"
 
 while True:
