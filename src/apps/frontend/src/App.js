14f1cf3d175b221b8fa12687ca8945baa1b6066b
Maximus Dan <43760171+danielroco@users.noreply.github.com>
2022-11-30 20:39:29 -0300

    Prototipe (#1)
    * Diseño inicial
    
    * falta la base de datos
    
    * Change gitignore
    
    * Added yarn.lock to gitignore
    
    Co-authored-by: fsalas21 <49110010+fsalas21@users.noreply.github.com>

--- src/apps/frontend/src/App.js
+++ src/apps/frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Event from './components/Event';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/events" element={<Event />} />
+            </Route>
+          </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/apps/frontend/src/components/Layout.js
@@ -0,0 +1,14 @@
+import NavigationBar from "./NavigationBar";
+import Home from "./Home";
+
+function Layout() {
+  
+  return (
+    <div>
+      <NavigationBar />
+      <Home />
+    </div>
+  );
+}
+
+export default Layout;
